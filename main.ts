e0a53ab042e99b3548885cdce2545b785b4b692c
Kristopher Hellsten <carvajalhellstenmail@gmail.com>
2025-04-21 00:21:24 +0200

    Add Express server setup with Deno

--- main.ts
+++ main.ts
@@ -1,8 +1,30 @@
-export function add(a: number, b: number): number {
-  return a + b;
-}
+// @ts-types="npm:@types/express@4.17.21"
+import express from "express";
 
-// Learn more at https://docs.deno.com/runtime/manual/examples/module_metadata#concepts
-if (import.meta.main) {
-  console.log("Add 2 + 3 =", add(2, 3));
-}
+const app = express();
+const PORT = 8000;
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.status(200).send({
+    message: "Hello world",
+    size: 10,
+  });
+});
+
+app.post("/:id", (req, res) => {
+  //
+  const { id } = req.params;
+  const { logo } = req.body;
+
+  if (!logo) {
+    res.status(418).send({ message: "We need a logo!" });
+  }
+
+  res.send({
+    bottle: `bottle with your ${logo} and ID of ${id}`,
+  });
+});
+
+app.listen(PORT, () => console.log(`it's alive on http://localhost:${PORT}`));
